dc35b7fadab97ec22d0d20972b820c569ad38738
Jorell Martis <jorell@tentwenty.me>
2023-02-20 17:43:06 +0530

    Added userbase connection logic nextauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,24 @@
+// @ts-nocheck
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import CredentialsProvider from "next-auth/providers/credentials";
+import connectMongo from '@/db/conn';
+import Users from '@/model/Schema';
+
 export default NextAuth({
     providers: [
     GoogleProvider({
         clientId: process.env.GOOGLE_ID!,
         clientSecret: process.env.GOOGLE_SECRET!,
     }),
+
+    CredentialsProvider({
+        name: "Credentials",
+        async authorize(credentials, req) {
+            connectMongo().catch(error => {error: "Connection Failed"})
+            //checking user existance
+            const result = await Users.findOne({email: credentials.email})
+        }
+        })
     ]
-  })
+})
--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,8 +1,33 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import connectMongo from '@/db/conn'
+import Users from '@/model/Schema';
+import { hash } from 'bcryptjs';
+import { Error } from 'mongoose';
 
 
 const handler = async(req: NextApiRequest, res: NextApiResponse) => {
     connectMongo().catch(error => res.json({error: "Connection Failed"}))
+
+    //only accepting POST method
+    if(req.method === 'POST'){
+        !req.body && res.status(404).json({error: "NO formww Data"});
+        const {email , password} = req.body;
+
+        //checking duplicate user
+        const checkExisting = await Users.findOne({email})
+        checkExisting && (
+            res.status(422).json({message: "User already exists"})
+        )
+        //Password BcryptJS
+        Users.create({email , password: await hash(password , 12)}, (err : Error ,data : NextApiResponse) => {
+            err && (
+                res.status(404).json({err})
+            )
+            res.status(201).json({status: true, user: data})
+        })
+    }
+    else{
+        res.status(500).json({message: 'HTTP method not valid'})
+    }
 }
 export default handler
